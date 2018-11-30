e1b30856101e116b71d08ae21904c15a267e192e
caicaihe <heliping_peter@163.com>
2018-11-30 17:53:34 +0900

    update

--- robotgogo/Haha/backend/robotExec.py
+++ robotgogo/Haha/backend/robotExec.py
@@ -3,7 +3,7 @@ import os
 
 def  runRobot():
     robotScrptDir = "~/mygithub/quality/automation/rf/compass/devops/测试用例/API/devopsAPI.txt"
-    resultDir = "/opt/rfresult/devops/report"
+    resultDir = "/opt/rfresult/devops/result"
     command = "robot --outputdir"+"  "+resultDir+" "+robotScrptDir
     print(command)
     output = os.system(command)
